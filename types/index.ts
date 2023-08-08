e37781427f16a00c6b178bee58ee60690c185f70
ammarff <ammarff551@gmail.com>
2023-08-08 16:02:21 +0700

    update-card

---
+++ types/index.ts
@@ -0,0 +1,30 @@
+import { manufacturers } from './../constants/index';
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+    title: String;
+    containerStyles?: String;
+    handleClick?:MouseEventHandler<HTMLButtonElement>;
+    btnType?: "button" | "submit";
+    rightIcon?: string;
+}
+
+export interface SearchManufacturerProps {
+    manufacturer: String;
+    setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+    city_mpg: number;
+    class: string ;
+    combination_mpg: number;
+    cylinders:number;
+    displacement:number;
+    drive:string;
+    fuel_type:string;
+    highway_mpg:number;
+    make:string;
+    model:string;
+    transmission:string;
+    year:number
+}
